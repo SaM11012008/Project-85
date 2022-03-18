24057ddcc23cd1d0388cc079bf64e18cfe3b9a4d
SaM11012008 <kavitakhare77@gmail.com>
2022-03-18 21:59:38 +0530

    .

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,35 +1,63 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feed from "../screens/Feed";
-import CreatePost from "../screens/CreatePost";
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
+import Feed from '../screens/Feed';
+import CreatePost from '../screens/CreatePost';
 
-const Tab = createMaterialBottomTabNavigator()
+
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
+
     return (
         <Tab.Navigator
+            labeled={false}
+            barStyle={styles.bottomTabStyle}
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
-                    var iconName;
+                    var iconName
                     if (route.name === "Feed") {
-                        iconName = focused ? 'book' : 'book-outline'
-                    } else if (route.name === "CreatePost ") {
-                        iconName = focused ? 'create' : 'create-outline'
+                        iconName = focused
+                            ? "book"
+                            : "book-outline";
+                    } else if (route.name === "CreatePost") {
+                        iconName = focused
+                            ? "create"
+                            : "create-outline";
                     }
-                    return <Ionicons name={iconName} size={size} color={color} />
-                }
+                    return (<Ionicons name={iconName} size={RFValue(25)} color={color} style={styles.icons} />);
+                },
             })}
             tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray'
-            }}
-        >
+                activeTintColor: "#ee8249",
+                inactiveTintColor: "gray"
+            }}>
+
             <Tab.Screen name="Feed" component={Feed} />
             <Tab.Screen name="CreatePost" component={CreatePost} />
+
         </Tab.Navigator>
-    )
+    );
 }
 
+
+
 export default BottomTabNavigator;
+
+
+const styles = StyleSheet.create({
+    bottomTabStyle: {
+        backgroundColor: "#2f345d",
+        height: "8%",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        overflow: "hidden",
+        position: "absolute"
+    },
+    icons: {
+        width: RFValue(30),
+        height: RFValue(30)
+    }
+});
--- screens/PostCard.js
+++ screens/PostCard.js
@@ -3,52 +3,148 @@ import {
     View,
     Text,
     StyleSheet,
+    SafeAreaView,
     Platform,
     StatusBar,
     Image,
     Dimensions
 } from "react-native";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { RFValue } from "react-native-responsive-fontsize";
 
+let customFonts = {
+    "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
 export default class PostCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        };
+    }
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+    }
+
+    componentDidMount() {
+        this._loadFontsAsync();
+    }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.authorContainer}>
-                        <View style={styles.authorImageContainer}>
-                            <Image source={require("../assets/profile_img.png")}
-                                style={styles.profileImage} />
+        if (!this.state.fontsLoaded) {
+            return <AppLoading />;
+        } else {
+            return (
+
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <View style={styles.cardContainer}>
+                        <View style={styles.storyImage}>
+                            <Image source={require("../assets/image_6.jpg")} style={{
+                                resizeMode: "contain", width: Dimensions.get("window").width - 60, height: 250, borderRadius: 10
+                            }}></Image>
+
                         </View>
-                        <View style={styles.authorNameContainer}>
-                            <Text style={styles.authorNameText}>
-                                {this.props.posts.author}
+                        <View style={styles.titleContainer}>
+                            <View style={styles.titleTextContainer}>
+                                <View style={styles.storyTitle}>
+                                    <Text style={styles.storyTitleText}>
+                                        {this.props.post.title}
+                                    </Text>
+                                </View>
+                                <View style={styles.storyAuthor}>
+                                    <Text style={styles.storyAuthorText}>
+                                        {this.props.post.author}
+                                    </Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.descriptionContainer}>
+                            <Text style={styles.descriptionText}>
+                                {this.props.post.created_on}
                             </Text>
                         </View>
-                    </View>
-                    <Image source={require("../assets/post.jpeg")} style={styles.postImage} />
-                    <View style={styles.captionContainer}>
-                        <Text style={styles.captionText}>
-                            {this.props.posts.caption}
-                        </Text>
-                    </View>
-                    <View style={styles.actionContainer}>
-                        <View style={styles.likeButton}>
-                            <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                            <Text style={styles.likeText}>12k</Text>
+                        <View style={styles.actionContainer}>
+                            <View style={styles.likeButton}>
+                                <View style={styles.likeIcon}>
+                                    <Ionicons name={"heart"} size={30} color={"#ffffff"} style={{
+                                        width: 30, marginLeft: 20, marginTop: 5
+                                    }}></Ionicons>
+                                </View>
+                                <View>
+                                    <Text style={styles.likeText}>
+                                        12k
+                                    </Text>
+                                </View>
+                            </View>
                         </View>
-                    </View >
-                </View >
-            </View >
+                    </View>
+                </View>
 
-        )
+            );
+        }
     }
 }
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
+    },
+    cardContainer: {
+        margin: RFValue(13),
+        backgroundColor: "#2f345d",
+        borderRadius: RFValue(20)
+    },
+    storyImage: {
+        resizeMode: "contain",
+        width: "95%",
+        alignSelf: "center",
+        height: RFValue(250)
+    },
+    titleContainer: {
+        paddingLeft: RFValue(20),
+        justifyContent: "center"
+    },
+    storyTitleText: {
+        fontSize: RFValue(25),
+        fontFamily: "Bubblegum-Sans",
+        color: "white"
+    },
+    storyAuthorText: {
+        fontSize: RFValue(18),
+        fontFamily: "Bubblegum-Sans",
+        color: "white"
+    },
+    descriptionText: {
+        fontFamily: "Bubblegum-Sans",
+        fontSize: 13,
+        color: "white",
+        paddingTop: RFValue(10)
+    },
+    actionContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: RFValue(10)
+    },
+    likeButton: {
+        width: RFValue(160),
+        height: RFValue(40),
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        backgroundColor: "#eb3948",
+        borderRadius: RFValue(30)
+    },
+    likeText: {
+        color: "white",
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(25),
+        marginLeft: RFValue(5)
     }
-})
+});
